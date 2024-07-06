52ce744913642bc2462bf7acd5deb40c758987b5
fuad78 <mdfuad4646@gmail.com>
2024-07-06 22:06:49 +0600

    seceond time change

--- app.js
+++ app.js
@@ -26,3 +26,41 @@ menu_item.forEach((item) => {
 		mobile_menu.classList.toggle('active');
 	});
 });
+// end section
+document.addEventListener('DOMContentLoaded', () => {
+	const blogContainer = document.querySelector('.all-blog-posts');
+  
+	// Example blog posts data
+	const blogPosts = [
+	  {
+		title: "Troubleshooting Common Hardware Issues: A Beginner's Guide",
+		author: "Mike Rodriguez",
+		description: "Hardware issues can be daunting for beginners. This guide covers common hardware problems and provides step-by-step solutions to help you troubleshoot and fix these issues efficiently....",
+		link: "https://www.pluralsight.com/blog/tutorials/troubleshooting-hardware"
+	  },
+	  {
+		title: "Top 10 Cybersecurity Practices for Small Businesses",
+		author: "KM FUAD HASAN",
+		description: "Hardware issues can be daunting for beginners. This guide covers common hardware problems and provides step-by-step solutions to help you troubleshoot and fix these issues efficiently....",
+		link: "https://www.fortinet.com/resources/cyberglossary/10-cybersecurity-tips-small-business"
+	  }
+	  // Add more blog posts here
+	];
+  
+	blogPosts.forEach(post => {
+	  const blogPost = document.createElement('div');
+	  blogPost.classList.add('blog-post');
+  
+	  blogPost.innerHTML = `
+		<div class="blog-info">
+		  <h1>${post.title}</h1>
+		  <h2>${post.author}</h2>
+		  <p>${post.description}</p>
+		  <a href="${post.link}" class="read-more">Read More</a>
+		</div>
+	  `;
+  
+	  blogContainer.appendChild(blogPost);
+	});
+  });
+  
